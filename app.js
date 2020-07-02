d9b051f6e0513f2f42c598fbf81fe292701cbd24
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2020-07-02 19:58:13 +0530

    adding data, methods and data-binding

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Gagandeep'
+        name: 'Gagandeep',
+        about: 'Chill guy',
+        website: 'http://github.com/gagandeepahuja09',
+        websiteTag: '<a href="http://github.com/gagandeepahuja09">Way of binding html written in js</a>',
+    },
+    methods: {
+        greet: function(time) {
+            return "Good " + time + ', ' + this.name; 
+        }
     }
 });
