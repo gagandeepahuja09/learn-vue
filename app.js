293e904387a45e2c5ccdca01abff5d761a158efc
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2020-07-02 22:30:14 +0530

    using directives for listening to events in vue

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ new Vue({
     data: {
         name: 'Gagandeep',
         about: 'Chill guy',
+        age: 21,
         website: 'http://github.com/gagandeepahuja09',
         websiteTag: '<a href="http://github.com/gagandeepahuja09">Way of binding html written in js</a>',
     },
     methods: {
         greet: function(time) {
             return "Good " + time + ', ' + this.name; 
+        },
+        add: function(inc) {
+            return this.age += inc;
+        },
+        subtract: function() {
+            return this.age--;
         }
     }
 });
