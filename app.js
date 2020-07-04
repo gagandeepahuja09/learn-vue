759c242a332b139940c4aadc46fa468036d5ec0a
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2020-07-04 16:43:50 +0530

    simple game dynamic css style

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 new Vue({
     el: '#vue-app',
     data: {
-        groceries: [ "bread", "jam", "mango", "banana", "butter", "cheese", "apple", "pickle" ],
-        friends: [
-            { 
-                name: "Sabhan",
-                age: 22
-            },
-            {
-                name: "Gagandeep",
-                age: 21
-            },
-            {
-                name: "Awasthi",
-                age: 23
-            },
-        ]
+        height: 300,
+        ended: false,
     },
     methods: {
+        kick: function() {
+            if(this.height >= 30)
+                this.height -= 30;
+            if(this.height == 0)
+                this.ended = true;
+        },
+        restart: function() {
+            this.height = 300;
+            this.ended = false;
+        }
     },
     computed: {
     }
